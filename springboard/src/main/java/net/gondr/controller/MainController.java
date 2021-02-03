64e79f066033bdb3850d66102cb230e28075b0bf
jinu0107 <wjdrms735@naver.com>
2021-02-03 12:49:44 +0900

    점심시간

--- springboard/src/main/java/net/gondr/controller/MainController.java
+++ springboard/src/main/java/net/gondr/controller/MainController.java
@@ -1,13 +1,18 @@
 package net.gondr.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-	@RequestMapping(value="/", method=RequestMethod.GET)
+	@GetMapping("/")
 	public String mainPage() {
+		return "main";
+	}
+	
+	@GetMapping("/test")
+	public String sample(@RequestParam(value="age" , required=false , defaultValue="10") int age) {
 		return "main";
 	}
 }
