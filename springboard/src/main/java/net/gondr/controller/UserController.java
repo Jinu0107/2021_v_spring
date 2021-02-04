4c4dd58f2415e819d0fd1ff46792067f7fa48484
jinu0107 <wjdrms735@naver.com>
2021-02-04 10:12:40 +0900

    회원가입 구현

--- springboard/src/main/java/net/gondr/controller/UserController.java
+++ springboard/src/main/java/net/gondr/controller/UserController.java
@@ -9,36 +9,45 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.fasterxml.jackson.databind.annotation.JsonAppend.Attr;
-
 import net.gondr.domain.RegisterDTO;
-import net.gondr.service.UserServiceImpl;
+import net.gondr.domain.UserVO;
+import net.gondr.service.UserService;
 import net.gondr.util.FileUtil;
 
 @Controller
 @RequestMapping("/user/")
 public class UserController {
-	
+
 	@Autowired
-	private UserServiceImpl service;
-	
+	private UserService service;
+
 	@Autowired
 	private ServletContext context;
-	
+
 	@GetMapping("register")
 	public String viewRegisterPage() {
 		return "user/register";
 	}
-	
+
 	@PostMapping("register")
-	public String registerProcess(RegisterDTO dto) throws Exception{
+	public String registerProcess(RegisterDTO dto) throws Exception {
 		String uploadPath = context.getRealPath("/WEB-INF/upload");
-		System.out.println(uploadPath);
-		
+//		System.out.println(uploadPath);
+
 		MultipartFile file = dto.getProfileImg();
-		String upFile = FileUtil.uploadFile(uploadPath, file.getOriginalFilename(), file.getBytes());
-		System.out.println(uploadPath + "에 " + upFile + "로 업로드 되었습니다.");
-		return "user/register";
+		String upFile = "";
+//		업로드한 파일이 존재하면
+		if (!file.getOriginalFilename().equals("")) {
+			upFile = FileUtil.uploadFile(uploadPath, file.getOriginalFilename(), file.getBytes());
+		}
+		UserVO user = new UserVO();
+		user.setImg(upFile);
+		user.setName(dto.getUsername());
+		user.setPassword(dto.getPassword());
+		user.setUserid(dto.getUserid());
+		service.register(user);
+
+		return "redirect:/";
 	}
 
 }
